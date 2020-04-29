0e22372bf9d5dfbccbf700705e3dc6012603dc25
Eric Le Goff <eric.legoff@sonarsource.com>
2020-04-29 11:23:27 +0200

    added code in branch

--- src/main/java/com/sonarsource/support/Reproducer.java
+++ src/main/java/com/sonarsource/support/Reproducer.java
@@ -3,6 +3,10 @@ package com.sonarsource.support;
     public class Reproducer {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int i=42;
+        System.out.println("A magic number is " + i);
+
     }
 }
     
