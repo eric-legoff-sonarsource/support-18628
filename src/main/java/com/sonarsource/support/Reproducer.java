0bfa288e206c5ae6dbcf1547509229b9db326671
Eric Le Goff <eric.legoff@sonarsource.com>
2020-04-30 18:25:03 +0200

    add code smell in b2

--- src/main/java/com/sonarsource/support/Reproducer.java
+++ src/main/java/com/sonarsource/support/Reproducer.java
@@ -1,8 +1,30 @@
 package com.sonarsource.support;
-    
-    public class Reproducer {
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reproducer {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public String readFile(File f) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+          FileReader fileReader = new FileReader("fileName");
+          BufferedReader bufferedReader = new BufferedReader(fileReader);
+      
+          while((line = bufferedReader.readLine()) != null) {
+            //...
+          }
+        }
+        catch (IOException e) {  // Noncompliant
+          throw e;
+        }
+        return sb.toString();
+      }
 }
     
