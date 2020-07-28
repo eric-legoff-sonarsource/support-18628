da3d92df1baeb2b075351e8e838313c0cdf7f6f9
Eric Le Goff <elegoff@protonmail.com>
2020-07-28 10:37:04 +0200

    fp

--- src/main/java/com/sonarsource/support/Reproducer.java
+++ src/main/java/com/sonarsource/support/Reproducer.java
@@ -1,8 +1,23 @@
 package com.sonarsource.support;
+
+import java.util.List;
+
+import jdk.internal.net.http.frame.ResetFrame;
     
     public class Reproducer {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static double result;
+
+	public static void main(final String[] args) {
+       result = compute();
+       System.out.println(result);
+    }
+
+    private static double compute() {
+        double res = return  List.of(1, 2, 3).stream().mapToLong(i -> {
+            return i * i;
+        }).sum();
+        return res;
+        
     }
 }
     
